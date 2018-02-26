51cf99582ec6f0a39cc49dd28b8e5b798b003e54
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-02-26 22:30:34 +0200

    [#5] Add jsoup dependency
    https://github.com/jhy/jsoup/

--- src/main/java/psychic/guide/GuideApplication.java
+++ src/main/java/psychic/guide/GuideApplication.java
@@ -1,12 +1,27 @@
 package psychic.guide;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GuideApplication {
 
 	public static void main(String[] args) {
+//		Document document = fetch("https://www.techradar.com/news/mobile-computing/laptops/best-laptops-1304361");
+
 		SpringApplication.run(GuideApplication.class, args);
+	}
+
+	private static Document fetch(String url) {
+		try {
+			return Jsoup.connect(url).get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
