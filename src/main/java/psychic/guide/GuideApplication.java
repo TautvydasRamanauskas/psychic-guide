301361dc60b91ae5a7924193b1fb6727fcc11170
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-02-26 22:52:43 +0200

    [#12] Add brand names list

--- src/main/java/psychic/guide/GuideApplication.java
+++ src/main/java/psychic/guide/GuideApplication.java
@@ -5,13 +5,20 @@ import org.jsoup.nodes.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class GuideApplication {
 
 	public static void main(String[] args) {
 //		Document document = fetch("https://www.techradar.com/news/mobile-computing/laptops/best-laptops-1304361");
+//		Set<String> brandList = fetchBrandList();
 
 		SpringApplication.run(GuideApplication.class, args);
 	}
@@ -23,5 +30,15 @@ public class GuideApplication {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	private static Set<String> fetchBrandList() {
+		Path path = new File("brands").toPath();
+		try {
+			return Files.lines(path).collect(Collectors.toSet());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Collections.emptySet();
 	}
 }
