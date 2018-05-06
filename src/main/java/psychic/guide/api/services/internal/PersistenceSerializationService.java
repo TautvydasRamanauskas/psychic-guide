f5aed05d6bddae81d5f1e05b0bad0b0879db07de
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-05-06 23:44:16 +0300

    [#28] Encrypt data files

--- src/main/java/psychic/guide/api/services/internal/PersistenceSerializationService.java
+++ src/main/java/psychic/guide/api/services/internal/PersistenceSerializationService.java
@@ -1,9 +1,15 @@
 package psychic.guide.api.services.internal;
 
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-public class PersistenceSerializationService<T> implements PersistenceService<T> {
+public class PersistenceSerializationService<T extends Serializable> implements PersistenceService<T> {
 	private static final String FILE_NAME_FORMAT = "data/%s.ser";
+	private static final String CIPHER = "Blowfish";
+	private static final SecretKey KEY = new SecretKeySpec(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}, CIPHER);
 	private final String fileName;
 
 	public PersistenceSerializationService(String fileName) {
@@ -12,23 +18,43 @@ public class PersistenceSerializationService<T> implements PersistenceService<T>
 
 	@Override
 	public synchronized void save(T object) {
-		try (FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-			 ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream)) {
-			outputStream.writeObject(object);
-		} catch (IOException e) {
+		try {
+			Cipher cipher = getCipher(Cipher.ENCRYPT_MODE);
+			SealedObject sealedObject = new SealedObject(object, cipher);
+			try (ObjectOutputStream outputStream =
+						 new ObjectOutputStream(
+								 new CipherOutputStream(
+										 new BufferedOutputStream(
+												 new FileOutputStream(fileName)), cipher))) {
+				outputStream.writeObject(sealedObject);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public T read() {
-		try (FileInputStream fileInputStream = new FileInputStream(fileName);
-			 ObjectInputStream inputStream = new ObjectInputStream(fileInputStream)) {
-			//noinspection unchecked
-			return (T) inputStream.readObject();
+		try {
+			Cipher cipher = getCipher(Cipher.DECRYPT_MODE);
+			try (ObjectInputStream inputStream =
+						 new ObjectInputStream(
+								 new CipherInputStream(
+										 new BufferedInputStream(
+												 new FileInputStream(fileName)), cipher))) {
+				SealedObject sealedObject = (SealedObject) inputStream.readObject();
+				//noinspection unchecked
+				return (T) sealedObject.getObject(cipher);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	private static Cipher getCipher(int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+		Cipher cipher = Cipher.getInstance(CIPHER);
+		cipher.init(encryptMode, KEY);
+		return cipher;
 	}
 }
