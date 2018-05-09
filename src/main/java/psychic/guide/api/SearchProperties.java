15efe5665b7c2a94ce517652d56b99d26c428177
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-05-09 22:33:27 +0300

    [#32] Fix .properties path

--- src/main/java/psychic/guide/api/SearchProperties.java
+++ src/main/java/psychic/guide/api/SearchProperties.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class SearchProperties {
+	private static final String FILE_PATH = "src/main/resources/search.properties";
 	private static final SearchProperties INSTANCE = new SearchProperties();
 	private final Properties properties;
 
@@ -20,7 +21,7 @@ public class SearchProperties {
 
 	private static Properties loadProperties() {
 		Properties properties = new Properties();
-		try (FileInputStream inputStream = new FileInputStream(new File("."))) {
+		try (FileInputStream inputStream = new FileInputStream(new File(FILE_PATH))) {
 			properties.load(inputStream);
 		} catch (IOException e) {
 			e.printStackTrace();
