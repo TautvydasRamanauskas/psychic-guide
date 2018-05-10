0164e4e6ea1be6e2ab1cb3ab930c1b6e2db90ad0
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-05-10 15:23:52 +0300

    [#23] Permit OPTIONS request

--- src/main/java/psychic/guide/api/services/security/WebSecurity.java
+++ src/main/java/psychic/guide/api/services/security/WebSecurity.java
@@ -1,6 +1,7 @@
 package psychic.guide.api.services.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,7 +13,8 @@ import psychic.guide.api.SearchProperties;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
+		http.csrf().disable().cors().disable()
+				.authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll()
 				.anyRequest().authenticated()
 				.and().httpBasic();
 	}
