ad116b3ff82450fcca8e96a1056f026481e6fadb
TautvydasRa <ta.ramanauskas@ktu.edu>
2018-05-10 16:57:00 +0300

    [#34] Persist data on separate thread

--- src/main/java/psychic/guide/api/services/internal/PersistenceSerializationService.java
+++ src/main/java/psychic/guide/api/services/internal/PersistenceSerializationService.java
@@ -5,53 +5,49 @@ import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PersistenceSerializationService<T extends Serializable> implements PersistenceService<T> {
 	private static final String FILE_NAME_FORMAT = "data/%s.ser";
 	private static final String CIPHER = "Blowfish";
 	private static final SecretKey KEY = new SecretKeySpec(new byte[]{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}, CIPHER);
 	private final String fileName;
+	private final ExecutorService saveExecutor;
+	private final Object accessLock;
 
 	public PersistenceSerializationService(String fileName) {
 		this.fileName = String.format(FILE_NAME_FORMAT, fileName);
+		this.saveExecutor = Executors.newSingleThreadExecutor();
+		this.accessLock = new Object();
 	}
 
 	@Override
-	public synchronized void save(T object) {
-		try {
-			Cipher cipher = getCipher(Cipher.ENCRYPT_MODE);
-			SealedObject sealedObject = new SealedObject(object, cipher);
-			try (ObjectOutputStream outputStream =
-						 new ObjectOutputStream(
-								 new CipherOutputStream(
-										 new BufferedOutputStream(
-												 new FileOutputStream(fileName)), cipher))) {
-				outputStream.writeObject(sealedObject);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public void save(T object) {
+		saveExecutor.execute(() -> saveWithLock(object));
 	}
 
 	@Override
 	public T read() {
-		try {
-			Cipher cipher = getCipher(Cipher.DECRYPT_MODE);
-			try (ObjectInputStream inputStream =
-						 new ObjectInputStream(
-								 new CipherInputStream(
-										 new BufferedInputStream(
-												 new FileInputStream(fileName)), cipher))) {
-				SealedObject sealedObject = (SealedObject) inputStream.readObject();
-				//noinspection unchecked
-				return (T) sealedObject.getObject(cipher);
-			}
-		} catch (EOFException | FileNotFoundException ignored) {
+		synchronized (accessLock) {
+			try {
+				Cipher cipher = getCipher(Cipher.DECRYPT_MODE);
+				try (ObjectInputStream inputStream =
+							 new ObjectInputStream(
+									 new CipherInputStream(
+											 new BufferedInputStream(
+													 new FileInputStream(fileName)), cipher))) {
+					SealedObject sealedObject = (SealedObject) inputStream.readObject();
+					//noinspection unchecked
+					return (T) sealedObject.getObject(cipher);
+				}
+			} catch (EOFException | FileNotFoundException ignored) {
 
-		} catch (Exception e) {
-			e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
 		}
-		return null;
 	}
 
 	@Override
@@ -61,6 +57,24 @@ public class PersistenceSerializationService<T extends Serializable> implements 
 			return defaultResult;
 		}
 		return read;
+	}
+
+	private void saveWithLock(T object) {
+		synchronized (accessLock) {
+			try {
+				Cipher cipher = getCipher(Cipher.ENCRYPT_MODE);
+				SealedObject sealedObject = new SealedObject(object, cipher);
+				try (ObjectOutputStream outputStream =
+							 new ObjectOutputStream(
+									 new CipherOutputStream(
+											 new BufferedOutputStream(
+													 new FileOutputStream(fileName)), cipher))) {
+					outputStream.writeObject(sealedObject);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private static Cipher getCipher(int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
